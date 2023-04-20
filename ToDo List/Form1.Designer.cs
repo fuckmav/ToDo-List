16db03b313dfd8aae0d60327e6053e1b4c090077
mav <130927590+fuckmav@users.noreply.github.com>
2023-04-20 12:58:57 +0100

    Add files via upload

---
+++ ToDo List/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ToDo_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 33);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(100, 405);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ToDo List";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(118, 59);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(118, 113);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 3;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(118, 172);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 4;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Job";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(118, 33);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(151, 20);
+            this.Input.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 450);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "               ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Input;
+    }
+}
+
---
+++ ToDo List/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            refresh();
+        }
+
+        void refresh()
+        {
+            StreamReader sr = new StreamReader("ToDo_List.txt");
+
+            Output.Text = sr.ReadToEnd();
+
+            sr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            StreamReader copy = new StreamReader("ToDo_List.txt");
+            
+            string cc = copy.ReadToEnd();
+
+            copy.Close();
+
+
+
+            StreamWriter sw = new StreamWriter("ToDo_List.txt");
+
+
+            sw.Write(cc);
+
+            sw.WriteLine(Input.Text);
+
+            sw.Close();
+
+            refresh();
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            string search = Input.Text;
+
+            Input.Text = "";
+
+            var tempFile = Path.GetTempFileName();                                         
+            var linesToKeep = File.ReadLines("ToDo_List.txt").Where(l => l != search);
+
+            File.WriteAllLines(tempFile, linesToKeep);
+            
+            File.Delete("ToDo_List.txt");
+
+            File.Move(tempFile, "ToDo_List.txt");
+
+            refresh();
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+
+            StreamWriter sw = new StreamWriter("ToDo_List.txt");
+
+            sw.Write("");
+
+            sw.Close();
+
+            refresh();
+
+        }
+
+        private void Output2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
